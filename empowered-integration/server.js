e0c0480c5e3887a9eedafa69a7de0b1a4bae4ae8
Antony-Q <antonyq1992@gmail.com>
2025-07-25 16:41:37 -0500

    Initial server.js

---
+++ empowered-integration/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const webhookRoutes = require('./routes/webhook');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use('/webhook', webhookRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
