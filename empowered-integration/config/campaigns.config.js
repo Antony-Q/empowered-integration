364e1986b137437efe1cfa8c3058f1d5d445a5ba
Antony-Q <antonyq1992@gmail.com>
2025-07-31 21:25:51 -0500

    created the campaign configuration file we will be needing to store the lead form format and content. More campaigns can be added to it in the future.

---
+++ empowered-integration/config/campaigns.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'test': {
+    list: 'Island ENT-Exomind',
+    fields: ['first_name', 'last_name', 'email', 'number1'],
+    F9CallASAP: true,
+    F9updateCRM: true
+  }
+  // Add more campaigns, as needed, below this line
+};
